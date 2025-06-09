0b9aa9e59815f430fb082f219ad5e51f683e2f7b
KayrougeDev <litrib@proton.me>
2025-06-09 10:08:35 +0200

    first commit

---
+++ src/main/java/fr/kayrouge/brave/BRAVEDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.kayrouge.brave;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BRAVEDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
